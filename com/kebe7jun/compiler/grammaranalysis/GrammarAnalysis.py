ba2630554fb19095add16fd599e8adab6f4ef2a9
KEBE <kebe0412@hotmail.com>
2016-05-30 00:53:30 +0800

    Grammar analysis.

--- com/kebe7jun/compiler/grammaranalysis/GrammarAnalysis.py
+++ com/kebe7jun/compiler/grammaranalysis/GrammarAnalysis.py
@@ -14,7 +14,7 @@ class GrammarAnalysis():
 			"if":self.deal_if,
 			"for":self.deal_for,
 			"repeat":self.deal_repeat,
-			"whil":self.deal_while,
+			"while":self.deal_while,
 			"const":self.deal_const,
 			"var":self.deal_var,
 			"begin":self.deal_begin
@@ -41,7 +41,7 @@ class GrammarAnalysis():
 		}
 
 	def start_analysis(self):
-		self.analysis_line()
+		self.out_index = 4
 		if not self.deal_program():
 			self.result['error'] = 'Can\'t find the enterance of the program.\n'
 		while not self.is_end():
@@ -55,21 +55,32 @@ class GrammarAnalysis():
 						self.deal_instruct()
 		return self.result
 
+	def print_info(self, w):
+		for i in range(self.out_index):
+			self.result['result'] += ' '
+		self.result['result'] += w + '\n'
+
 	def analysis_line(self):
 		pass
 
 	def deal_program(self):
-		return self.get_next_token()['key'] == 1 and self.is_symbol(self.get_next_token())
+		self.print_info('Analysis the program head.')
+		r = self.get_next_token()['key'] == 1 and self.is_symbol(self.get_next_token())
+		self.print_info('Analysis the program head done.')
+		return r
 
 	def deal_const(self):
 		print 'Analysing const instruct...'
+		self.print_info('Analysing const area...')
 		t = self.get_next_token()
 		is_need_deal = True
+		self.out_index += 4
 		while not self.is_end() and t['value'] not in ('var', 'begin'):
 			if not is_need_deal:
 				t = self.get_next_token()
 				continue
 			if self.is_var(t):
+				self.print_info('Analysing const...')
 				tmp = {}
 				tmp['name'] = t['value']
 				t = self.get_next_token()
@@ -96,25 +107,35 @@ class GrammarAnalysis():
 							self.write_error(t['line'], 'Already define the const name \'{}\''.format(tmp['name']))
 						t = self.get_next_token()
 		self.return_before_token()
+		self.out_index -=4
+		self.print_info('Const area analysis done.')
 
 	def deal_begin(self):
 		print 'Analysing begin instruct...'
+		self.print_info('Analysing start part...')
 		t = self.get_next_token()
+		self.out_index += 4
 		while not self.is_end() and t['value'] not in ('end', 'end.'):
 			if t['value'] != ';':
 				try:
-					func = self.module_func[t['value']]
-					func()
+					self.module_func[t['value']]()
 				except Exception:
 					if self.is_symbol(t):
+						self.return_before_token()
 						self.deal_instruct()
+			t = self.get_next_token()
+		self.out_index -=4
+		if t['value'] in ('end', 'end.'):
+			self.print_info('Start part analysis done.')
 		self.return_before_token()
-		pass
+
 
 	def deal_var(self):
 		print 'Analysing var instruct...'
+		self.print_info('Analysing var area...')
 		t = self.get_next_token()
 		is_need_anas = True
+		self.out_index += 4
 		while t['value'] not in ('begin'):
 			if not is_need_anas:
 				t = self.get_next_token()
@@ -131,12 +152,15 @@ class GrammarAnalysis():
 						'type':'',
 						'used_times':0
 					})
-					if not self.is_valid_const(tmp[-1]['name']):
-						self.write_error(t['line'], 'Already define the name \'{}\' as a const.'.format(t['value']))
-						del tmp[-1]
-					elif not self.is_valid_var(tmp[-1]['name']):
-						self.write_error(t['line'], 'Already define the name \'{}\' as a var.'.format(t['value']))
-						del tmp[-1]
+					try:
+						if not self.is_valid_const(tmp[-1]['name']):
+							self.write_error(t['line'], 'Already define the name \'{}\' as a const.'.format(t['value']))
+							del tmp[-1]
+						elif not self.is_valid_var(tmp[-1]['name']):
+							self.write_error(t['line'], 'Already define the name \'{}\' as a var.'.format(t['value']))
+							del tmp[-1]
+					except Exception,e:
+						pass
 					t = self.get_next_token()
 					if t['value'] == ':':
 						break
@@ -149,7 +173,8 @@ class GrammarAnalysis():
 			if t['value'] == ':':
 				t = self.get_next_token()
 				if self.is_var_type(t):
-					self.var_table.append([{'name':x['name'], 'type':t['value'], 'used_times':x['used_times']} for x in tmp])
+					self.print_info('Analysing {} type var...'.format(t['value']))
+					([self.var_table.append({'name':x['name'], 'type':t['value'], 'used_times':x['used_times']}) for x in tmp])
 					t = self.get_next_token()
 					if t['value'] != ';':
 						self.write_error(t['line'], 'Unexpect end of var define, missing \';\'')
@@ -160,21 +185,29 @@ class GrammarAnalysis():
 					self.write_error(t['line'], 'Unexpect var type {}'.format(t['value']))
 					is_need_anas = False
 		self.return_before_token()
+		self.out_index -= 4
+		self.print_info('Var area analysis done.')
 
-	def deal_express(self):
-		t = self.get_next_token()
+	def deal_express(self, is_in_b = False):
+ 		t = self.get_next_token()
 		dealed_list = []
+		self.print_info('Analysing express...')
 		while not self.is_end() and not self.is_key_word(t):
 			if self.is_var(t) or self.is_const(t):
+				if self.is_var(t):
+					if  self.is_valid_var(t['value']) and self.is_valid_const(t['value']):
+						self.write_error(t['line'], 'Undefined var \'{}\''.format(t['value']))
 				dealed_list.append(t)
 				t = self.get_next_token()
+				if t['value'] == ')' and is_in_b:
+					self.return_before_token()
+					return
 				if not self.is_middle_symbol(t):
 					# Isn't valid express
-					if not self.is_key_word(t):
-						self.write_error(t['line'], 'Invalid str {}.'.format(t['value']))
-					while not self.is_end() and not self.is_key_word(t):
-						t = self.get_next_token()
+					# if not self.is_key_word(t):
+					# 	self.write_error(t['line'], 'Invalid str \'{}\'.'.format(t['value']))
 					self.return_before_token()
+					self.read_to_instruct_end()
 					break
 				dealed_list.append(t)
 				t = self.get_next_token()
@@ -182,25 +215,44 @@ class GrammarAnalysis():
 				# 	type = self.deal_bracket()
 			elif t['value'] in ('(', ')'):
 				type = self.deal_bracket()
+				t = self.get_next_token()
+				if not self.is_middle_symbol(t):
+					# Isn't valid express
+					# if not self.is_key_word(t) or t['value'] != ';':
+					# 	self.write_error(t['line'], 'Invalid str \'{}\'.'.format(t['value']))
+					# while not self.is_end() and not self.is_key_word(t):
+					# 	t = self.get_next_token()
+					self.return_before_token()
+					break
+				dealed_list.append(t)
+				t = self.get_next_token()
+				# t = self.get_next_token()
 			elif t['value'] in ('+', '-'):
 				t = self.get_next_token()
-				if not self.is_const(t) or not self.is_var(t):
+				if not self.is_const(t) and not self.is_var(t):
 					# Isn't valid express
 					if not self.is_key_word(t):
-						self.write_error(t['line'], 'Invalid str {}.'.format(t['value']))
+						self.write_error(t['line'], 'Invalid str \'{}\'.'.format(t['value']))
 					while not self.is_end() and not self.is_key_word(t):
 						t = self.get_next_token()
 					self.return_before_token()
 					break
+			else:
+				self.write_error(t['line'], 'Invalid str \'{}\'.'.format(t['value']))
+				while not self.is_end() and not self.is_key_word(t):
+						t = self.get_next_token()
 
 		print dealed_list
-		pass
+		self.print_info('Express analysis done.')
 
 	def deal_bracket(self):
 		t = self.get_next_token()
-		while t['value'] != ')':
+		while t['value'] != ')' and not self.is_key_word(t):
 			if t['value'] == '(':
 				self.deal_bracket()
+			else:
+				self.return_before_token()
+				self.deal_express(True)
 			t = self.get_next_token()
 		pass
 
@@ -209,20 +261,89 @@ class GrammarAnalysis():
 			Here set the priority that
 			( + - * / ) > (and or not) > (> < >= <= <>)
 		'''
+		if self.is_end():
+			return
+		self.print_info('Analysing IF setence...')
+		self.out_index += 4
 		self.deal_express()
-		pass
+		t = self.get_next_token()
+		if t['value'] == 'then':
+			self.deal_instruct()
+			t = self.get_next_token()
+			if t is not None and t['value'] == 'else':
+				self.deal_instruct()
+			else:
+				self.return_before_token()
+		else:
+			self.write_error(t['line'], 'Missing \'then\' at line {}.'.format(t['line']))
+		self.out_index -= 4
+		self.print_info('If setence analysis done.')
+		# self.return_before_token()
 
 	def deal_for(self):
 		pass
 
 	def deal_while(self):
-		pass
+		self.print_info('Analysing WHILE setence...')
+		self.out_index += 4
+		self.deal_express()
+		t = self.get_next_token()
+		if t['value'] == 'begin':
+			self.deal_begin()
+		else:
+			while not self.is_key_word(t):
+				self.deal_instruct()
+		self.out_index -= 4
+		self.print_info('While setence analysis done.')
 
 	def deal_repeat(self):
 		pass
 
 	def deal_instruct(self):
-		pass
+		if self.is_end():
+			return
+		self.print_info('Analysing instruct...')
+		self.out_index += 4
+		t = self.get_next_token()
+		if not self.is_var(t):
+			self.write_error(t['line'], 'Invalid instruct start with \'{}\''.format(t['value']))
+			#self.read_to_instruct_end()
+			return
+		if self.is_valid_var(t['value']) and self.is_valid_const(t['value']):
+			self.write_error(t['line'], 'Undefined var \'{}\''.format(t['value']))
+		t = self.get_next_token()
+		if t['value'] == ':':
+			t = self.get_next_token()
+			if t['value'] != '=':
+				self.write_error(t['line'], 'Missing \'=\' before \'{}\''.format(t['value']))
+				#self.read_to_instruct_end()
+				return
+			else:
+				# such as a := 1;
+				t = self.get_next_token()
+				if not self.is_const(t):
+					# is not const
+					self.write_error(t['line'], '\'{}\' is not a const.'.format(t['value']))
+					#self.read_to_instruct_end()
+					return
+		elif t['value'] == '=':
+			# such as a = 1+23/23;
+			self.deal_express()
+			t = self.get_next_token()
+			if t['value'] != ';':
+				self.write_error(t['line'], 'Unexcept end of instruct at \'{}\''.format(t['value']))
+				self.return_before_token()
+
+		self.out_index -= 4
+		self.print_info('Instruct setence analysis done.')
+
+
+
+	def read_to_instruct_end(self):
+		t = self.get_next_token()
+		while t['value'] != ';' and not self.is_key_word(t):
+			t = self.get_next_token()
+		self.return_before_token()
 
 	#Read the next token info
 	def get_next_token(self):
@@ -241,7 +362,7 @@ class GrammarAnalysis():
 		return self.now_token_index >= len(self.token)
 
 	def is_key_word(self, t):
-		return t['key'] <= 20
+		return t['key'] <= 17
 
 	def is_symbol(self, t):
 		return t['key'] == 34
@@ -259,7 +380,10 @@ class GrammarAnalysis():
 		return t['key'] == 34
 
 	def is_const(self, t):
-		return 35 <= t['key'] <= 38
+		return 35 <= t['key'] <= 38 or 18 <= t['key'] <= 20
+
+	def is_user_set_const(self, t):
+		return
 
 	def is_var_type(self, t):
 		return 3<=t['key'] <=6
@@ -272,10 +396,12 @@ class GrammarAnalysis():
 		print err
 
 	def is_valid_const(self, c):
-		return not [x for x in self.const_table if x['name'] == c]
+		x = not [x for x in self.const_table if x['name'] == c]
+		return x
 
 	def is_valid_var(self, c):
-		return not [x for x in self.var_table if x['name'] == c]
+		x = not [x for x in self.var_table if x['name'] == c]
+		return x
 
 	def is_middle_symbol(self, t):
 		return 22<=t['key'] <=33
